b3572e2a4fd81f54326b5a9bd2e889866ecd9f62
Doaa Sayed Ibrahim Morsy <76880107+doaasa@users.noreply.github.com>
2022-10-06 12:13:01 +0200

    Add files via upload

---
+++ BookAuthorTask/Migrations/20220831072408_intial.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookAuthorTask.Migrations
+{
+    public partial class intial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "authors",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_authors", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sections",
+                columns: table => new
+                {
+                    sectionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sections", x => x.sectionID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "book",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    pages = table.Column<int>(type: "int", nullable: false),
+                    authorID = table.Column<int>(type: "int", nullable: false),
+                    sectionid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_book", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_book_authors_authorID",
+                        column: x => x.authorID,
+                        principalTable: "authors",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_book_sections_sectionid",
+                        column: x => x.sectionid,
+                        principalTable: "sections",
+                        principalColumn: "sectionID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_book_authorID",
+                table: "book",
+                column: "authorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_book_sectionid",
+                table: "book",
+                column: "sectionid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "book");
+
+            migrationBuilder.DropTable(
+                name: "authors");
+
+            migrationBuilder.DropTable(
+                name: "sections");
+        }
+    }
+}
---
+++ BookAuthorTask/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookAuthorTask
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int pages { get; set; }
+        public Author author { get; set; }
+        public int authorID { get; set; }
+
+        public section Section { get; set; }
+        public int sectionid { get; set; }
+    }
+    public class Author
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public List <Book> books { get; set; }
+    }
+    public class section
+    {
+        public int sectionID { get; set; }
+        public string name { get; set; }
+        public List<Book> books { get; set; }
+
+    }
+    public class BookContext : DbContext
+    {
+        public DbSet<Book>book { get; set; }
+        public DbSet<Author> authors { get; set; }
+        public DbSet<section> sections { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"
+                           Server=DESKTOP-SFDKHS6\SQLEXPRESS;
+                           Database=AuthorBook;
+                           Trusted_Connection=True;");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            section c = new section() { name = "programming" };
+            Author a = new Author() { name = "doaa", age = 30 };
+            List<Book> b = new List<Book>(){
+               new Book() {  name="book1", pages=320,authorID=8, sectionid=1 },
+                new Book() {  name="book2", pages=20,authorID=8, sectionid=1 },
+                 new Book() {  name="book3", pages=520,authorID=8, sectionid=1 }
+
+        };
+            
+
+            BookContext boo = new BookContext();
+            //boo.book.AddRange(b);
+             var ans = boo.book.FirstOrDefault(x => x.authorID == 8);
+            ans.name = "dd";
+            boo.book.Update(ans);
+            //boo.book.Remove(ans);
+            boo.SaveChanges();
+
+
+
+
+
+            Console.WriteLine("Done");
+        }
+    }
+}
